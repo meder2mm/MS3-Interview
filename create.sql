a11ca2cc8fc636738f57944948168e480745b4d7
meder2mm <36486632+meder2mm@users.noreply.github.com>
2018-03-22 17:03:27 -0400

    Create file DDL
    Create file for MS3 database

---
+++ create.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE MS3;
+USE MS3;
+DROP TABLE IF EXISTS values;
+CREATE TABLE values (
+    A integer NOT NULL,
+    B text NOT NULL,
+    C text NOT NULL,
+    D text NOT NULL,
+    E text NOT NULL,
+    F text NOT NULL,
+    G text NOT NULL,
+    H text NOT NULL,
+    I text NOT NULL,
+    J text NOT NULL
+);
