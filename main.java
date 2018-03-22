3ea5cb1ca52bf606685fa01098f82f41325f911a
meder2mm <36486632+meder2mm@users.noreply.github.com>
2018-03-22 18:29:22 -0400

    Update main.java

--- main.java
+++ main.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 public class main {
 
   public static void main(String[] args) throws Exception {
-    Connection connection = DriverManager.getConnection("jdbc:sqlite:C:/sqlite/MS3.db");
+    Connection connection = DriverManager.getConnection("jdbc:sqlite:C:/MS3.db");
     CSVLoader loader = new CSVLoader(connection);
     loader.loadCSV("C:\\ms3Interview.csv", "data", true);
     loader.createStatistics();
